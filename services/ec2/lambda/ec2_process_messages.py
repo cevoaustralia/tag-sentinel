affa1c8e4d8f25179dc16aade7d24fabdf9ad61c
Orlando Erazo <erazomartinez@gmail.com>
2016-09-12 21:19:24 +1000

    - Add concret classe for managing ec2 messages, abstract methods to be implemented - Add new files required for lambda function in make file

--- services/ec2/lambda/ec2_process_messages.py
+++ services/ec2/lambda/ec2_process_messages.py
@@ -1,9 +1,38 @@
+import os
+import boto3
+from botocore import exceptions
+import logging
+import abc
+import sys
+import json
+#When testing lambda locally
+sys.path.append('../../../common')
+from base_processor import base_processor
+
+class ec2_process_messages(base_processor) :
+    """
+    Concrete class to process SQS messages for EC2 instances
+    """
+    def __init__(self):
+        self.config = {}
+        self.sns_region = None
+        #Initialize parent class
+        super(ec2_process_messages,self).__init__()
+
+    def get_interesting_messages(self):
+        logging.info("Get relevant messages to be implemented here")
+
+    def tag_or_delete(self):
+        logging.info("Tag or delete to be implemented here")
 
 def lambda_handler(event, context):
     """
-    Handler to be invoked from lambda
+    Handler to be invoked from aws lambda
     """
-    print "it is working..."
+    processor = ec2_process_messages()
+    processor.config_logging()
+    
+    logging.info("event: " + json.dumps(event))
 
 if __name__ == "__main__":
     lambda_handler({}, {})
